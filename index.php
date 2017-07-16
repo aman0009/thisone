de76a9b7e6e9ccb209c4840f757af2fcf26f7a92
hellloamman <amansharma33.as@gmail.com>
2017-07-17 00:04:10 +0530

    good

---
+++ index.php
@@ -0,0 +1,220 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>FizzFlyer</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/style.css" type="text/css" rel="stylesheet" media="all">   
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" property="" />   
+<!-- //Custom Theme files -->
+<!-- font-awesome icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="js/jquery-2.2.3.min.js"></script> 
+<!-- //js -->
+<!-- web-fonts --> 
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link href="http://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">
+<!-- //web-fonts --> 
+</head>
+<body>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-30027142-1', 'w3layouts.com');
+  ga('send', 'pageview');
+</script>
+<script async type='text/javascript' src='http://cdn.fancybar.net/ac/fancybar.js?zoneid=1502&amp;serve=C6ADVKE&amp;placement=w3layouts' id='_fancybar_js'></script>
+
+<body>
+
+
+	<!-- banner -->
+	<div class="banner">  
+		<div class="header agileinfo-header"><!-- header -->
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<h1 style="color: orange;"><a href="index.php" style="color: orange;">FizzFlyer</span></a></h1>
+					</div> 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-left"> 
+							<li><a href="index.php" class="w3ls-hover active">Home</a></li>
+							<li><a href="tours.php" class="btn w3ls-hover">Tours</a></li>   
+							<li><a href="iv.html" class="btn w3ls-hover">Inustrial Visits</a></li>
+							<li><a href="about.html" class="btn w3ls-hover">About us</a></li>  
+							<li><a href="contact.html" class="btn w3ls-hover">Contact</a></li>
+						</ul>	   
+						<div class="social-icon">
+							<a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a> 
+							<a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>  
+
+						</div> 
+						<div class="clearfix"> </div>
+					</div><!-- //navbar-collapse --> 
+				</div><!-- //container-fluid -->
+			</nav>
+		</div><!-- //header -->	
+		<!-- banner-text -->
+		<div class="banner-text"> 
+			<div class="container">
+				<div class="flexslider">
+					<ul class="slides">
+						<li>
+							<div class="banner-w3lstext">
+								<h2>Make Your Experience Better</h2>
+								<p></p>
+							</div>
+					
+						</li>
+					</ul> 
+				</div> 	 
+			</div>
+		</div>
+		<!-- //banner-text -->    
+	</div>
+	<!-- //banner --> 
+	<!---728x90--->
+	<!-- welcome -->
+	<div class="welcome">
+		<div class="container">  
+			<div class="welcome-grids"> 
+				<div class="col-md-7 welcome-w3left">
+					<h4 style="font-family:cursive">FIZZFLYER</h4>
+					<p>FizzFlyer Pvt. Ltd. is the first of its kind trip planner start-up company.
+                         We are offering budget tours for group of enthusiasts and college goers with high quality transport and stay.</p>  
+				</div>
+				<div class="col-md-5 welcome-w3right">
+					<img src="images/img1.jpg" class="img-responsive" alt="" />
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<!-- //welcome -->
+	<!---728x90--->
+	<!--- albums -->
+	<div class="albums"> 
+		<div class="w3lalbums-grid">
+			<div class="col-md-6 col-sm-6 albums-left"> 
+				<div class="wthree-almubimg">  
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 albums-right">
+				<h4>Our Latest Trips - Pondicherry</h4>
+				<p>Pondicherry (or Puducherry), a French colonial settlement in India until 1954, is now a Union Territory town bounded by the southeastern Tamil Nadu state. Its French legacy is preserved in its French Quarter, with tree-lined streets.</p>
+			</div>
+			<div class="clearfix"></div>
+		</div> 
+		<div class="w3lalbums-grid">
+			<div class="col-md-6 col-sm-6 albums1-right"> 
+				<div class="wthree-almubimg wthree1-almubimg">  
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 albums1-left">
+				<h4>Our Latest Trips - Kodaikanal</h4>
+				<p>Pondicherry (or Puducherry), a French colonial settlement in India until 1954, is now a Union Territory town bounded by the southeastern Tamil Nadu state. Its French legacy is preserved in its French Quarter, with tree-lined streets.</p>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>  
+	</div>
+
+	
+	<div class="newsletter">
+		<div class="container">
+			<h3 class="agileits-title">Services</h3>
+			<p>Mauris est odio laoreet laoreet sapien non, sollicitudin bibendum nulla amet purus sodales blandit.</p>
+			<form action="#" method="post"> 
+				
+				<div class="clearfix"> </div> 
+			</form> 
+		</div> 
+	</div>
+	<!-- //newsletter -->
+	<!-- footer start here --> 
+	<div class="footer-agile">
+		<div class="container">
+			<div class="footer-agileinfo"> 
+				<div class="col-md-5 col-sm-5 footer-wthree-grid"> 
+					<div class="agileits-w3layouts-tweets">  
+						<h5><a href="index.html">FizzFlyer</a></h5> 
+						<div class="social-icon footerw3ls">
+							<a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a> 
+						<a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>  
+						</div>
+					</div>
+					<p>FizzFlyer Pvt. Ltd. is the first of its kind trip planner start-up company.
+                         We are offering budget tours for group of enthusiasts and college goers with high quality transport and stay.</p>
+				</div> 
+				<div class="col-md-3 col-sm-3 footer-wthree-grid">
+					<h3>Quick Links</h3>
+					<ul>
+						<li><a href="index.php"><span class="glyphicon glyphicon-menu-right"></span> Home</a></li>
+						<li><a href="about.html"><span class="glyphicon glyphicon-menu-right"></span> About</a></li> 
+						<li><a href="tours.php"><span class="glyphicon glyphicon-menu-right"></span> Tours</a></li>
+						<li><a href="contact.html"><span class="glyphicon glyphicon-menu-right"></span> Contact</a></li>
+					</ul>
+				</div> 	 
+				<div class="col-md-4 col-sm-4 footer-wthree-grid">
+					<h3>Contact Details:</h3>
+					<ul>
+						<li>SRM University</li> 
+						<li>Q-404 Abode Valley</li>
+						<li>Phone: +91 7987199909</li>
+						<li><a href=""> amansharma33.as@gmail.com</a></li>
+					</ul>
+				</div>
+				<div class="clearfix"> </div>		
+			</div>
+			<div class="copy-right"> 
+				<p>© 2017 FizzFlyer . All rights reserved | Design by <a href="https://amanportfolio.herokuapp.com/" target="_blank">Aman Sharma</a></p>
+			</div>
+		</div>
+	</div> 
+	<!-- //footer end here --> 
+	
+	<!-- FlexSlider --> 
+	<script defer src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript">
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+	</script>
+	<!-- End-slider-script --> 
+	<!-- start-smooth-scrolling -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>	
+	<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+			
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+	</script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
