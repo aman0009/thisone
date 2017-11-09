e138c49fc67851506c32cd1fbdfcca2e8e7dcedf
hellloamman <amansharma33.as@gmail.com>
2017-11-09 15:13:39 +0530

    make it better

--- index.php
+++ index.php
@@ -9,11 +9,14 @@
 <!-- Custom Theme files -->
 
 <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
-<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
 <link href="css/style.css" type="text/css" rel="stylesheet" media="all">   
+<link href="css/style3.css" type="text/css" rel="stylesheet" media="all"> 
+<link href="css/top.css" type="text/css" rel="stylesheet" media="all">  
 <link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" property="" />   
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 <!-- //Custom Theme files -->
 <!-- font-awesome icons -->
 <link href="css/font-awesome.css" rel="stylesheet"> 
@@ -21,20 +24,28 @@
 <!-- js -->
 <script src="js/jquery-2.2.3.min.js"></script> 
 <!-- //js -->
+
+  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css'>
 <!-- web-fonts --> 
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 <link href="http://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">
 <!-- //web-fonts --> 
 </head>
+<style type="text/css">
+	.row
+	{
+		margin:0px;
+		padding: 0px;
+	}
+img {
+    display: block;
+    margin: 0 auto;
+}
+
+</style>
 <body>
-<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'></script><script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-30027142-1', 'w3layouts.com');
-  ga('send', 'pageview');
-</script>
+
 <script async type='text/javascript' src='http://cdn.fancybar.net/ac/fancybar.js?zoneid=1502&amp;serve=C6ADVKE&amp;placement=w3layouts' id='_fancybar_js'></script>
 
 <body>
@@ -53,16 +64,16 @@ ga('create', 'UA-30027142-1', 'w3layouts.com');
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<h1 style="color: orange;"><a href="index.php" style="color: orange;">FizzFlyer</span></a></h1>
+						<h1 style="color: orange;"><a href="index.php" style="color: orange; font-family: 'Bad Script', cursive;">FizzFlyer</span></a></h1>
 					</div> 
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav navbar-left"> 
 							<li><a href="index.php" class="w3ls-hover active">Home</a></li>
 							<li><a href="tours.php" class="btn w3ls-hover">Tours</a></li>   
-							<li><a href="#" class="btn w3ls-hover">Inustrial Visits</a></li>
+							<li><a href="iv.html" class="btn w3ls-hover">Inustrial Visits</a></li>
 							<li><a href="about.php" class="btn w3ls-hover">About us</a></li>  
-							<li><a href="#" class="btn w3ls-hover">Contact</a></li>
+							<li><a href="contact.html" class="btn w3ls-hover">Contact</a></li>
 						</ul>	   
 						<div class="social-icon">
 							<a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a> 
@@ -92,86 +103,213 @@ ga('create', 'UA-30027142-1', 'w3layouts.com');
 		</div>
 		<!-- //banner-text -->    
 	</div>
-	<!-- //banner --> 
-	<!---728x90--->
-	<!-- welcome -->
-	<div class="welcome">
-		<div class="container">  
-			<div class="welcome-grids"> 
-				<div class="col-md-7 welcome-w3left">
-					<h4 style="font-family:cursive">OUR STORY</h4>
-					<p>As college students down south, our only summer respite was weekend trips to the hill stations Ooty and Kodaikanal. These trips although refreshing, were haphazardly planned and often exceeded our budgets. This being a common problem, we came up with Fizzflyer offering student centric trips complete with budget stay and travel with the necessary impetus on the adventure.</p>  
-				</div>
-				<div class="col-md-5 welcome-w3right">
-					<img src="images/img1.jpg" class="img-responsive" alt="" />
-				</div>
-				<div class="clearfix"></div>
-			</div>
-		</div>
-	</div>
-	<!-- //welcome -->
-	<!---728x90--->
-	<!--- albums -->
-	<div class="albums"> 
-		<div class="w3lalbums-grid">
-			<div class="col-md-6 col-sm-6 albums-left"> 
-				<div class="wthree-almubimg">  
-				</div>
-			</div>
-			<div class="col-md-6 col-sm-6 albums-right">
-				<h4>Our Latest Trips - Pondicherry</h4>
-				<p>Pondicherry (or Puducherry), a French colonial settlement in India until 1954, is now a Union Territory town bounded by the southeastern Tamil Nadu state. Its French legacy is preserved in its French Quarter, with tree-lined streets.</p>
-			</div>
-			<div class="clearfix"></div>
-		</div> 
-		<div class="w3lalbums-grid">
-			<div class="col-md-6 col-sm-6 albums1-right"> 
-				<div class="wthree-almubimg wthree1-almubimg">  
-				</div>
-			</div>
-			<div class="col-md-6 col-sm-6 albums1-left">
-				<h4>Our Latest Trips - Kodaikanal</h4>
-				<p>Pondicherry (or Puducherry), a French colonial settlement in India until 1954, is now a Union Territory town bounded by the southeastern Tamil Nadu state. Its French legacy is preserved in its French Quarter, with tree-lined streets.</p>
-			</div>
-			<div class="clearfix"></div>
-		</div>
-		<div class="clearfix"></div>  
-	</div>
-
 	
-	<div class="newsletter">
+
+
+
+<section class="container tm-home-section-1" id="more">
+
+		</div>
+	
+		<div class="section-margin-top">
+			<div class="row">				
+				<div class="tm-section-header">
+					<div class="col-lg-3 col-md-3 col-sm-3"><hr></div>
+					<div class="col-lg-6 col-md-6 col-sm-6"><h2 class="tm-section-title" style="margin: 5px; font-family: sans-serif;">TOP DEALS</h2></div>
+					<div class="col-lg-3 col-md-3 col-sm-3"><hr></div>	
+				</div>
+				<img src="images/baza.jpeg" style="height: 500px; margin-top: 5px;">
+			</div>
+		
+					
+		</div>
+	</section>		
+
+<section class="tm-white-bg section-padding-bottom">
 		<div class="container">
-			<h3 class="agileits-title">Services</h3>
-			<i class="material-icons" style="font-size: 60px;">hotel</i>
-			<i class="material-icons" style="font-size: 60px;">directions_bus</i>
-			<i class="material-icons" style="font-size: 60px;">rowing</i>
-			<i class="material-icons" style="font-size: 60px;">directions_bike</i>
-			<p style="color: #213;">ACCOMODATION</p>
-			<p style="color: #213;">TRAVEL AND TRANSPORT</p>
-			<p style="color: #213;">ADVENTURES/THRILL ACTIVITIES</p>
-            <p style="color: #213;">RENTAL BIKES AND CARS</p>
-			<form action="#" method="post"> 
-				
-				<div class="clearfix"> </div> 
-			</form> 
-		</div> 
-	</div>
+			<div class="row">
+				<div class="tm-section-header section-margin-top">
+					<div class="col-lg-4 col-md-3 col-sm-3"><hr></div>
+					<div class="col-lg-4 col-md-6 col-sm-6"><h2 class="tm-section-title" style="font-family: sans-serif;">Popular Packages</h2></div>
+					<div class="col-lg-4 col-md-3 col-sm-3"><hr></div>	
+				</div>
+				<div class="col-lg-6">
+					<div class="tm-home-box-3">
+						<div class="tm-home-box-3-img-container">
+							<img src="img/index-07.jpg" alt="image" class="img-responsive">	
+						</div>						
+						<div class="tm-home-box-3-info">
+						<h3 style="margin: 5px;">KODAIKANAL</h3>
+							<p class="tm-home-box-3-description">Some description</p>
+					        <div class="tm-home-box-2-container">
+							<a href="#" class="tm-home-box-2-link"></i></a>
+							<a href="#" class="tm-home-box-2-link"><span class="tm-home-box-2-description box-3">Price Starting:3999</span></a>
+							<a href="#" class="tm-home-box-2-link"></i></a>
+						</div>
+						</div>						
+					</div>					
+			     </div>
+			     <div class="col-lg-6">
+			        <div class="tm-home-box-3">
+						<div class="tm-home-box-3-img-container">
+							<img src="img/index-08.jpg" alt="image" class="img-responsive">	
+						</div>						
+						<div class="tm-home-box-3-info">
+						<h3>Ooty</h3>
+							<p class="tm-home-box-3-description">Some description</p>
+					        <div class="tm-home-box-2-container">
+							<a href="#" class="tm-home-box-2-link"></i></a>
+							<a href="#" class="tm-home-box-2-link"><span class="tm-home-box-2-description box-3">Price Starting:4999</span></a>
+							<a href="#" class="tm-home-box-2-link"></i></a>
+						</div>
+						</div>						
+					</div>
+				</div>
+				<div class="col-lg-6">
+				    <div class="tm-home-box-3">
+						<div class="tm-home-box-3-img-container">
+							<img src="img/index-09.jpg" alt="image" class="img-responsive">	
+						</div>						
+						<div class="tm-home-box-3-info">
+						<h3>Coorg</h3>
+							<p class="tm-home-box-3-description">Some Description</p>
+					        <div class="tm-home-box-2-container">
+							<a href="#" class="tm-home-box-2-link"></i></a>
+							<a href="#" class="tm-home-box-2-link"><span class="tm-home-box-2-description box-3">Starting Price:4499</span></a>
+							<a href="#" class="tm-home-box-2-link"></i></a>
+						</div>
+						</div>						
+					</div>
+			    </div>
+			    <div class="col-lg-6">
+			        <div class="tm-home-box-3">
+						<div class="tm-home-box-3-img-container">
+							<img src="img/index-10.jpg" alt="image" class="img-responsive">	
+						</div>						
+						<div class="tm-home-box-3-info">
+						<h3>Pondichery</h3>
+							<p class="tm-home-box-3-description">Starting at 2999</p>
+					        <div class="tm-home-box-2-container">
+							<a href="#" class="tm-home-box-2-link"></i></a>
+							<a href="#" class="tm-home-box-2-link"><span class="tm-home-box-2-description box-3">Starting Price:2999</span></a>
+							<a href="#" class="tm-home-box-2-link"></i></a>
+						</div>
+						</div>						
+					</div>
+			   	</div>
+			</div>		
+		</div>
+	</section>
+
+
+
+
+
+
+
+
+
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <h2 class="module-title font-alt" style=" margin: 5px; color: black;">Services</h2>
+            </div>
+        </div>
+        <!-- /MODULE TITLE -->
+
+        <div class="row multi-columns-row">
+
+            <!-- CONTENT BOX -->
+            <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="content-box">
+                    <div class="content-box-icon">
+                        <i class="ion-android-bus"></i>
+                    </div>
+                    <h5 class="content-box-title font-alt">ACCOMODATION</h5>
+                    <div class="content-box-text">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis.
+                    </div>
+                </div>
+            </div>
+            <!-- /CONTENT BOX -->
+
+            <!-- CONTENT BOX -->
+            <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="content-box">
+                    <div class="content-box-icon">
+                        <i class="ion-ios-gear-outline"></i>
+                    </div>
+                    <h5 class="content-box-title font-alt">TRAVEL AND TRANSPORT</h5>
+                    <div class="content-box-text">
+                        Nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.
+                    </div>
+                </div>
+            </div>
+            <!-- /CONTENT BOX -->
+
+            <!-- CONTENT BOX -->
+            <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="content-box">
+                    <div class="content-box-icon">
+                        <i class="ion-ios-monitor-outline"></i>
+                    </div>
+                    <h5 class="content-box-title font-alt">ADVENTURES/THRILL ACTIVITIES</h5>
+                    <div class="content-box-text">
+                        Vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
+                    </div>
+                </div>
+            </div>
+            <!-- /CONTENT BOX -->
+
+            <!-- CONTENT BOX -->
+            <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="content-box">
+                    <div class="content-box-icon">
+                        <i class="ion-ios-lightbulb-outline"></i>
+                    </div>
+                    <h5 class="content-box-title font-alt">RENTAL BIKES AND CARS</h5>
+                    <div class="content-box-text">
+                        Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus bus tincidunt.
+                    </div>
+                </div>
+            </div>
+            <!-- /CONTENT BOX -->
+
+            <!-- CONTENT BOX -->
+            <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="content-box">
+                    <div class="content-box-icon">
+                        <i class="ion-ios-clock-outline"></i>
+                    </div>
+                    <h5 class="content-box-title font-alt">24/7 CUSTOMER SUPPORT</h5>
+                    <div class="content-box-text">
+                        Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+                    </div>
+                </div>
+            </div>
+            <!-- /CONTENT BOX -->
+
+            <!-- CONTENT BOX -->
+            <div class="col-sm-6 col-md-4 col-lg-4">
+                <div class="content-box">
+                    <div class="content-box-icon">
+                        <i class="ion-ios-chatbubble-outline"></i>
+                    </div>
+                    <h5 class="content-box-title font-alt">ONLINE SUPPORT</h5>
+                    <div class="content-box-text">
+                        Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum.
+                    </div>
+                </div>
+            </div>
+            <!-- /CONTENT BOX -->
+
+        </div>
+
+    </div>
 	<!-- //newsletter -->
 	<!-- footer start here --> 
-	<div class="footer-agile">
+		<div class="footer-agile">
 		<div class="container">
 			<div class="footer-agileinfo"> 
-				<div class="col-md-5 col-sm-5 footer-wthree-grid"> 
-					<div class="agileits-w3layouts-tweets">  
-						<h5><a href="index.html">FizzFlyer</a></h5> 
-						<div class="social-icon footerw3ls">
-							<a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a> 
-						<a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>  
-						</div>
-					</div>
-					<p>FizzFlyer Pvt. Ltd. is the first of its kind trip planner start-up company.
-                         We are offering budget tours for group of enthusiasts and college goers with high quality transport and stay.</p>
-				</div> 
 				<div class="col-md-3 col-sm-3 footer-wthree-grid">
 					<h3>Quick Links</h3>
 					<ul>
@@ -190,13 +328,42 @@ ga('create', 'UA-30027142-1', 'w3layouts.com');
 						<li><a href=""> amansharma33.as@gmail.com</a></li>
 					</ul>
 				</div>
-				<div class="clearfix"> </div>		
+				<div class="col-md-4 col-sm-4 footer-wthree-grid">
+					<h3>Partner With Us:</h3>
+					<ul>
+						<li><a href="registration.html" class="btn w3ls-hover">Being Hoteler</a></li> 
+						<li><a href="registration1.html" class="btn w3ls-hover">Being Traveller</a></li>
+					</ul>
+				</div>
+				
 			</div>
+			
+		</div>
+	</div> 
+
+
+
+		<div class="footer-agile">
+		<div class="container">
+			<div class="footer-agileinfo"> 
+				<div class="col-md-5 col-sm-5 footer-wthree-grid"> 
+					<div class="agileits-w3layouts-tweets">  
+						<h5 style="margin-top: 15px;"><a href="index.html">FizzFlyer</a></h5> 
+						<div class="social-icon footerw3ls" style="margin-top: 10px;">
+							<a href="#" class="social-button facebook" style="margin-top: 10px;"><i class="fa fa-facebook"></i></a> 
+						<a href="#" class="social-button google" style="margin-top: 10px;"><i class="fa fa-google-plus"></i></a>  
+						</div>
+					</div>
+					<p>FizzFlyer Pvt. Ltd. is the first of its kind trip planner start-up company.
+                         We are offering budget tours for group of enthusiasts and college goers with high quality transport and stay.</p>
+				</div> 
+			
 			<div class="copy-right"> 
-				<p>© 2017 FizzFlyer . All rights reserved | Design by <a href="https://amanportfolio.herokuapp.com/" target="_blank">Aman Sharma</a></p>
+				<p>© 2017 FizzFlyer . All rights reserved | Design by <a href="" target="_blank">Fizz Flyer and Company </a></p>
 			</div>
 		</div>
 	</div> 
+	
 	<!-- //footer end here --> 
 	
 	<!-- FlexSlider --> 
